8f344f2ebeeb23d63d1e2146947c8b89b324fcdb
AlbyAa <47159379+AlbyAa@users.noreply.github.com>
2022-10-09 12:29:44 +0200

    Add files via upload

--- tavolaPeriodica.py
+++ tavolaPeriodica.py
@@ -1,6 +1,7 @@
 
 import random
 
+'''
 elementi = {"H":"idrogeno", "He":"elio", "Li":"litio","Be":"berilio","B":"boro",
             "C":"carbonio",
             "N":"azoto",
@@ -73,15 +74,80 @@ elementi = {"H":"idrogeno", "He":"elio", "Li":"litio","Be":"berilio","B":"boro",
             "Ra":"radio",
             "Ac":"attinio"
             }
-elementiRand = random.sample(list(elementi),len(elementi))
 
+'''
+# elementi = {v: k for k, v in elementi.items()}
+
+'''
+elementi = { # VALENZE
+    "H":"1",
+    "Li":"1",
+    "Na":"1",
+    "K":"1",
+    "Cs":"1",
+    "Ag":"1",
+    "Mg":"2",
+    "Ca":"2",
+    "Sr":"2",
+    "Ba":"2",
+    "Zn":"2",
+    "Cd":"2",
+    "Al":"3",
+    "H":"-1",
+    "F":"-1",
+    "Cl":"-1",
+    "Br":"-1",
+    "I":"-1",
+    "O":"-2",
+    "S":"-2",
+    "N":"-3",
+    
+    "Cr":"2,3",
+    "Cb":"2,3",
+    "Fe":"2,3",
+    "Hg":"1,2",
+    "Pb":"2,4",
+    "Cu":"1,2",
+    "Sn":"2,4"
+}
+'''
+
+elementi = {
+    "NH4 +":"ammonio",
+    "H3O +":"idronio",
+    "CH3COO -":"acetato",
+    "CN -":"cianuro",
+    "OH -":"idrossido",
+    "ClO -":"ipoclorito",
+    "ClO2 -":"clorito",
+    "ClO3 -":"clorato",
+    "ClO4 -":"perclorato",
+    "NO2 -":"nitrito",
+    "NO3 -":"nitrato",
+    "MnO4 -":"permanganato",
+    "CO3 2-":"carbonato",
+    "HCO3 -":"idrogenocarbonato",
+    "CrO4 2-":"cromato",
+    "Cr2O7 2-":"dicromato",
+    "O2 2-":"perossido",
+    "PO4 3-":"fosfato",
+    "HPO4 2-":"idrogenofosfato",
+    "H2PO4 -":"diidrogenofosfato",
+    # "SO3 2-":"solfito",
+    # "SO4 2-":"solfato",
+    # "HSO4 -":"idrogenosolfato"
+}
+
+elementi = {v: k for k, v in elementi.items()}
+
+elementiRand = random.sample(list(elementi),len(elementi))
 
 def chiedi(boh):
     temp = {}
     for elemento in boh:
         print(elemento, end=" ")
         inp = input()
-        if(inp != elementi[elemento]):
+        if(inp.lower() != elementi[elemento].lower()):
             print(f"SBAGLIATO: {elementi[elemento]}")
             temp[elemento] = elementi[elemento]
     return temp        
@@ -92,11 +158,11 @@ def main():
     for elemento in elementiRand:
         print(elemento, end=" ")
         inp = input()
-        if(inp != elementi[elemento]):
+        if(inp.lower() != str(elementi[elemento]).lower()):
             print(f"SBAGLIATO: {elementi[elemento]}")
             sbagliati[elemento] = elementi[elemento]
     
-    while sbagliati is not None:
+    while len(sbagliati) != 0:
         sbagliati = chiedi(sbagliati)
     
 main()
